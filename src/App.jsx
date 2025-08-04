87cb3520d5a1421dda3408f7c36f4b7b0a71cdfd
yavanika22 <yavanikapopuri22@gmail.com>
2025-08-04 10:45:44 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <BrowserRouter basename="/ecommerce">
+    <BrowserRouter basename="/ecomJenkinsPractice">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
